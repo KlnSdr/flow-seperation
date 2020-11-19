71a3918386611e85db58505f87a34584f9ea3f33
KlnSdr <schneider.klabs@gmail.com>
2020-11-19 20:23:15 +0100

    * darstellen der gespeicherten Daten und Möglichkeit den Monat der Darstellung zu ändern

--- display.js
+++ display.js
@@ -2,6 +2,16 @@ let chartScriptLoaded = false;
 
 var charterJet;
 
+let headLineMonth = "";
+let monatsNamen = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+let data;
+
+let dataAbends = [];
+let dataMorgens = [];
+
+let displayYear;
+let displayMonth;
+
 function displayChart() {
     console.log("chartshow");
     if (chartScriptLoaded == false) {
@@ -10,107 +20,71 @@ function displayChart() {
             chartScriptLoaded = true;
 
             //=============================================================
-            let dataAbends = [600,
-                600,
-                625,
-                600,
-                600,
-                800,
-                650,
-                800,
-                790,
-                800,
-                800,
-                800,
-                800,
-                800,
-                800,
-                800,
-                650,
-                650,
-                600,
-                600,
-                650,
-                800,
-                700,
-                750,
-                650,
-                700,
-                650,
-                600,
-                600,
-                650,
-                800,
-                800,
-                800,
-                800,
-                700,
-                800,
-                700,
-                800,
-                800,
-                800,
-                800,
-                700,
-                700,
-                800,
-                700,
-                700,
-                800,
-                800
-            ];
-            let dataMorgens = [650,
-                800,
-                700,
-                800,
-                800,
-                800,
-                800,
-                700,
-                800,
-                700,
-                800,
-                800,
-                800,
-                800,
-                800,
-                800,
-                800,
-                700,
-                800,
-                800,
-                700,
-                700,
-                650,
-                780,
-                790,
-                680,
-                790,
-                700,
-                700,
-                800,
-                800,
-                700,
-                680,
-                750,
-                650,
-                800,
-                650,
-                800,
-                800,
-                800,
-                800,
-                800,
-                650,
-                700,
-                700,
-                700,
-                800,
-                800
-            ];
+            let json = JSON.parse(localStorage.getItem("flow-seperation"));
+
+            data = {};
+            for (let key of Object.keys(json)) {
+                let tmp = key.split(".");
+                let year = tmp[2];
+                let month = tmp[1];
+
+                if (data[year] != undefined) {
+                    if (data[year][month] != undefined) {
+                        data[year][month].push({
+                            value: json[key],
+                            date: key
+                        });
+                    } else {
+                        data[year][month] = [];
+                        data[year][month].push({
+                            value: json[key],
+                            date: key
+                        });
+                    }
+                } else {
+                    data[year] = {};
+                    data[year][month] = [];
+                    data[year][month].push({
+                        value: json[key],
+                        date: key
+                    });
+                }
+                console.log(data);
+            }
+
+            let date = new Date();
+
+            let currentMonth = data[date.getFullYear()][date.getMonth() + 1];
+
+            displayYear = date.getFullYear();
+            displayMonth = Object.keys(data[displayYear]).indexOf(((date.getMonth + 1 < 10) ? "0" : "") + (date.getMonth() + 1).toString());
+            console.log(Object.keys(data[displayYear]));
+            console.log(displayMonth);
+
+            console.log(data);
+
+
+            for (let day of currentMonth) {
+                if (day.value.abend == undefined || day.value.abend == null) {
+                    day.value.abend = day.value.morgen;
+                }
+
+                if (day.value.morgen == undefined || day.value.morgen == null) {
+                    day.value.morgen = day.value.abend;
+                }
+
+                dataAbends.push(day.value.abend);
+                dataMorgens.push(day.value.morgen);
+            }
+
+            headLineMonth = monatsNamen[currentMonth[0].date.split(".")[1] - 1] + " " + displayYear;
+
             let yData = [];
 
-            dataAbends.forEach(point => {yData.push("");});
+            dataAbends.forEach(point => {
+                yData.push("");
+            });
+
             charterJet = new Chart(document.getElementById('outputChart').getContext('2d'), {
                 type: 'line',
                 data: {
@@ -178,14 +152,17 @@ function displayChart() {
                         }]
                     },
                     legend: {
-                        position: 'bottom'
+                        position: 'bottom',
+                        labels: {
+                            fontColor: "white"
+                        }
                     },
                     tooltips: {
                         enabled: false
                     },
                     title: {
                         display: true,
-                        text: "Oktober",
+                        text: headLineMonth,
                         fontColor: "white"
                     },
                     animation: {
@@ -206,3 +183,83 @@ function displayChart() {
         charterJet = new Chart(document.getElementById('outputChart').getContext('2d'), options);
     }
 }
+
+function displayPreviouseMonth() {
+    console.log("prev");
+
+    let schlussel = Object.keys(data[displayYear]);
+    console.log(schlussel);
+    let previouse = null;
+
+    for (let i = 0; i < schlussel.length; i++) {
+        if (data[displayYear][schlussel[i]] != undefined) {
+            console.log(schlussel[i]);
+            console.log(schlussel[displayMonth]);
+
+            if (previouse == null) {
+                previouse = i;
+            } else {
+                if (parseInt(previouse) == parseInt(displayMonth) && parseInt(schlussel[i]) < parseInt(schlussel[displayMonth])) {
+                    previouse = i;
+                } else {
+                    if (parseInt(schlussel[previouse]) > parseInt(schlussel[displayMonth]) && parseInt(schlussel[i]) < parseInt(schlussel[displayMonth])) {
+                        previouse = i;
+                    } else {
+                        if (parseInt(schlussel[i]) < parseInt(schlussel[displayMonth]) && parseInt(schlussel[i]) > parseInt(schlussel[previouse])) {
+                            previouse = i;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let currentMonth = data[displayYear][schlussel[previouse]];
+    displayMonth = previouse;
+
+
+    dataAbends = [];
+    dataMorgens = [];
+    for (let day of currentMonth) {
+        if (day.value.abend == undefined || day.value.abend == null) {
+            day.value.abend = day.value.morgen;
+        }
+
+        if (day.value.morgen == undefined || day.value.morgen == null) {
+            day.value.morgen = day.value.abend;
+        }
+
+        dataAbends.push(day.value.abend);
+        dataMorgens.push(day.value.morgen);
+    }
+
+    headLineMonth = monatsNamen[currentMonth[0].date.split(".")[1] - 1] + " " + displayYear;
+
+    let yData = [];
+
+    dataAbends.forEach(point => {
+        yData.push("");
+    });
+
+    let options = charterJet.config;
+
+    options.data.datasets[0].data = dataMorgens;
+    options.data.datasets[1].data = dataAbends;
+    options.data.labels = yData;
+    options.options.scales.yAxes[0].ticks.maxTicksLimit = dataAbends.length;
+    options.options.scales.xAxes[0].ticks.maxTicksLimit = dataAbends.length;
+    options.options.title.text = headLineMonth;
+
+    charterJet.update(options);
+}
+
+function displayNextMonth() {
+    console.log("next");
+}
+
+// function saveChart() {
+//     console.log("save");
+//     document.getElementById("outputChart").toBlob((blob) => {
+//         saveAs("chart.png");;
+//     });
+// }
